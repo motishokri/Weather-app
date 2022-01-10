623807f492e2750842731c9035519e5261ea7b1b
motishokri <motahare.shokri@yahoo.com>
2022-01-10 20:28:06 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -85,4 +85,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("Tehran");
+searchCity("paris");
